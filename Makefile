d02a1652fed9a3e7edf73ee9fb752890b82c8f45
xdavio <xdavio@gmail.com>
2016-02-19 17:45:16 -0600

    positive table available document.pdf

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-document.pdf: table.R document.tex
+#sed -n 'H;$${x;s/begin{tabular.*\n/tiny\n\\&/;p;}' table.tex > tmp
+#rm -f table.tex
+#mv tmp table.tex
+
+Ztable.pdf: table.R document.tex
 	rm -f table.tex tmp
 	R --vanilla <table.R
-	sed -n 'H;$${x;s/begin{tabular.*\n/tiny\n\\&/;p;}' table.tex > tmp
-	rm -f table.tex
-	mv tmp table.tex
+
 	pdflatex document.tex
 	cp document.pdf Ztable.pdf
--- table.R
+++ table.R
@@ -1,6 +1,6 @@
 require(xtable)
 #outer vectors
-left <- seq(-2,2,by=.1)
+left <- seq(0,3.5,by=.1)
 up <- seq(0.00,.09,by=.01)
 
 join <- function(x,y) {
@@ -12,8 +12,10 @@ join <- function(x,y) {
 }
 
 Z <- round(outer(left,up,join),4)
-rownames(Z) <- as.character(round(left,1))
-colnames(Z) <- as.character(round(up,2))
+#rownames(Z) <- as.character(round(left,1))
+#colnames(Z) <- as.character(round(up,2))
+rownames(Z) <- format(round(left,1),nsmall=1)
+colnames(Z) <- format(round(up,2),nsmall=2)
 
 sink("table.tex")
 print(xtable(Z,digits=4))
